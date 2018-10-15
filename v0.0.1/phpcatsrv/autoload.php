2be94562f93006594c2f61031dab50fcd4f8427f
kissableandres <marcelo@giacaman.cl>
2018-10-15 14:21:28 -0300

    Fixes
    Correciones Menores en Comentarios

--- v0.0.1/phpcatsrv/autoload.php
+++ v0.0.1/phpcatsrv/autoload.php
@@ -8,6 +8,3 @@
 #--------------------------------------------------------------------------
 define('phpCat',1);
 #··········································································
-//	Configure correctamente las rutas {full_base_directory} y {appname}
-require('{full_base_directory}/apps/{appname}/config.php');
-require('{full_base_directory}/autoload.php');
